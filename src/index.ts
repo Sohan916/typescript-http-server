722d92970a8a8ca6b7e724b70edebd6ac771250a
sohan.io <sohan96973552@gmail.com>
2026-05-30 17:03:23 +0530

    Added metrics for api hits

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express, { type Request, type Response } from "express";
+import { config } from "./config.js";
 
+// Handlers.
 const handleReadiness = (req: Request, res: Response) => {
   res.set("Content-Type", "text/plain");
   res.status(200).send("OK");
 };
 
+const handleCounts = (req: Request, res: Response) => {
+  res.send(`Hits: ${config.fileserverHits}`);
+};
+
+const handleResetCount = (req: Request, res: Response) => {
+  config.fileserverHits = 0;
+  res.send();
+};
+
+// Middlewares.
 const middlewareLogResponses = (
   req: Request,
   res: Response,
@@ -20,13 +32,28 @@ const middlewareLogResponses = (
   next();
 };
 
+const middlewareMetricsInc = (
+  req: Request,
+  res: Response,
+  next: VoidFunction,
+) => {
+  config.fileserverHits++;
+  next();
+};
+
+// App.
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
 app.use(middlewareLogResponses);
 
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+
 app.get("/healthz", handleReadiness);
+
+app.get("/metrics", handleCounts);
+
+app.get("/reset", handleResetCount);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
