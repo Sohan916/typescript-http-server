b0ea0b6323ef5d8cf2c2d47fbc66954d93d6ba70
sohan.io <sohan96973552@gmail.com>
2026-05-30 12:08:48 +0530

    Added a healthz endpoint for checking readiness and updated static files path with app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
+
+const handleReadiness = (req: Request, res: Response) => {
+  res.set("Content-Type", "text/plain");
+  res.status(200).send("OK");
+};
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handleReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
