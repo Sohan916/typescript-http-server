c96c84de2933437e8ff0430f5182023b2b8ccf1d
sohan.io <sohan96973552@gmail.com>
2026-06-30 09:28:44 +0530

    Implement JWT and tested it

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,9 @@
 import { hash, verify } from "argon2";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+import { BadRequestError, UserNotAuthenticatedError } from "./app/errors.js";
+
+const TOKEN_ISSUER = "chirpy";
 
 export const hashPassword = (password: string): Promise<string> => {
   return hash(password);
@@ -10,3 +15,41 @@ export const checkPasswordHash = (
 ): Promise<Boolean> => {
   return verify(hash, password);
 };
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function makeJWT(userID: string, expiresIn: number, secret: string) {
+  const issuedAt = Math.floor(Date.now() / 1000);
+  const expiresAt = issuedAt + expiresIn;
+  const token = jwt.sign(
+    {
+      iss: TOKEN_ISSUER,
+      sub: userID,
+      iat: issuedAt,
+      exp: expiresAt,
+    } satisfies payload,
+    secret,
+    { algorithm: "HS256" },
+  );
+
+  return token;
+}
+
+export function validateJWT(tokenString: string, secret: string) {
+  let decoded: payload;
+  try {
+    decoded = jwt.verify(tokenString, secret) as JwtPayload;
+  } catch (e) {
+    throw new UserNotAuthenticatedError("Invalid token");
+  }
+
+  if (decoded.iss !== TOKEN_ISSUER) {
+    throw new UserNotAuthenticatedError("Invalid issuer");
+  }
+
+  if (!decoded.sub) {
+    throw new UserNotAuthenticatedError("No user ID in token");
+  }
+
+  return decoded.sub;
+}
